a69a070abf5e17c183ebeaa7af6a580c59eb113c
chensong <1953813160@qq.com>
2016-11-08 14:19:09 +0800

    Revert "add -lrt to solve undefined reference of clock_gettime in ev.c"
    This reverts commit b8aaf1ec11a74ab03163d1a405389abed5337a56.

--- build_arm.sh
+++ build_arm.sh
@@ -1,1 +1,1 @@
-arm-linux-gnueabi-gcc server.c cJSON.c jsonrpc-c.c -lev -lm -lrt -static -o lepd -I./arm-libev -L./arm-libev/
+arm-linux-gnueabi-gcc server.c cJSON.c jsonrpc-c.c -lev -lm -static -o lepd -I./arm-libev -L./arm-libev/
